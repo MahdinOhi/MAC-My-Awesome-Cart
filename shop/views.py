ecf0186cecc271c6237fdb1fbff31032a4bb56fc
ohi <mahdinislamohi@gmail.com>
2023-07-23 20:49:47 +0600

    Created Database Entry For Orders

--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,35 @@
-from django.shortcuts import render
-from .models import Product, Contact
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product, Contact, Orders
 from math import ceil
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 # Create your views here.
 
 
 def index(request):
-    products = Product.objects.all()
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
     allProds = []
     catprods = Product.objects.values('category', 'id')
-    cats = {item["category"] for item in catprods}
+    cats = {item['category'] for item in catprods}
     for cat in cats:
         prod = Product.objects.filter(category=cat)
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
 
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
     params = {'allProds': allProds}
-    return render(request, "shop/index.html", params)
+    return render(request, 'shop/index.html', params)
 
 
 def about(request):
@@ -25,15 +37,13 @@ def about(request):
 
 
 def contact(request):
-    if request.method == 'POST':
-        print(request)
+    if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-        print(name, email, phone, desc)
     return render(request, 'shop/contact.html')
 
 
@@ -42,15 +52,31 @@ def tracker(request):
 
 
 def search(request):
-    return render(request, "shop/search.html")
+    return render(request, 'shop/search.html')
 
 
 def productView(request, myid):
     # Fetch the product using the id
     product = Product.objects.filter(id=myid)
-    print(product)
+
     return render(request, 'shop/prodView.html', {'product': product[0]})
 
 
 def checkout(request):
-    return render(request, "shop/checkout.html")
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + \
+            " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
+    return render(request, 'shop/checkout.html')
