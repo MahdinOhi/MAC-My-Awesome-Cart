3f8ff8ec873d635937e50c42bbb5089accf71fd9
ohi <mahdinislamohi@gmail.com>
2023-07-26 13:51:11 +0600

    Created a Django Post Page For Displaying Blog Content

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Blogpost(models.Model):
     post_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=500)
     head0 = models.CharField(max_length=500, default="")
     chead0 = models.CharField(max_length=10000, default="")
     head1 = models.CharField(max_length=500, default="")
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from .models import Blogpost
 
 
 def index(request):
     return render(request, 'blog/index.html')
 
 
-def blogpost(request):
-    return render(request, 'blog/blogpost.html')
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id=id)[0]
+    print(post)
+    return render(request, 'blog/blogpost.html', {'post': post})
